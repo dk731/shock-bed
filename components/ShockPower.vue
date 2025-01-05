2d336981a860285a0c4c2c1bedc7516110721725
Demid Kaidalov <demid.kaidalov@gmail.com>
2025-01-06 00:28:53 +0200

    Enhance ShockPower component with toggle functionality and improve schedule management UI

--- components/ShockPower.vue
+++ components/ShockPower.vue
@@ -6,7 +6,13 @@ const shockPower = useSocketState<ShockPower>(`shockPower`, {
 </script>
 
 <template>
-  <div class="grid grid-cols-2 place-items-center p-2">
+  <div
+    class="grid grid-cols-2 place-items-center p-4 rounded"
+    :class="{
+      'animate-[pulse_1s_ease-in-out_infinite] border-2 border-warning':
+        shockPower.state.value?.isTesting,
+    }"
+  >
     <template v-if="shockPower.state.value !== undefined">
       <div class="w-full flex flex-col">
         <div class="mb-4">
@@ -22,13 +28,17 @@ const shockPower = useSocketState<ShockPower>(`shockPower`, {
           :step="0.01"
         />
       </div>
+
+      <button
+        class="btn"
+        @click="() => (shockPower.state.value!.isTesting = !shockPower.state.value!.isTesting)"
+      >
+        <span>Toggle Test </span>
+        <icon name="mdi:flash" size="25" />
+      </button>
     </template>
     <template v-else>
       <span class="loading loading-spinner loading-lg" />
     </template>
-    <button class="btn">
-      <span>Test</span>
-      <icon name="mdi:flash" size="25" />
-    </button>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,4 @@
 <script lang="ts" setup>
-import hardwareConfig from "#utils/hardware-configuration";
 import { DateTime, Duration } from "luxon";
 
 const registeredSchedules = useSocketState<ScheduleState[]>(
@@ -12,18 +11,26 @@ const registeredSchedules = useSocketState<ScheduleState[]>(
   ]
 );
 
-const schedules = computed({
-  get() {
-    return (
-      registeredSchedules.state.value?.map((el) => ({
-        ...el,
-        parsedTime: DateTime.fromISO(el.alarmTime),
-      })) || []
-    );
-  },
-  set(value: ScheduleState[]) {
-    // registeredSchedules.setState(value);
-  },
+const newShockScheduleMinutes = ref<{
+  minutes: number;
+  hours: number;
+}>({
+  minutes: 5,
+  hours: 0,
+});
+
+const schedules = computed(() => {
+  const parsedTimes =
+    registeredSchedules.state.value?.map((el) => ({
+      ...el,
+      parsedTime: DateTime.fromISO(el.alarmTime),
+    })) || [];
+
+  parsedTimes.sort((el1, el2) => {
+    return el1.parsedTime.diff(el2.parsedTime).as("milliseconds");
+  });
+
+  return parsedTimes;
 });
 
 const currentTime = ref(DateTime.now());
@@ -35,6 +42,22 @@ async function onScheduleClick(scheduleId: string) {
     (el) => el.scheduleId !== scheduleId
   );
 }
+
+async function onScheduleAddClick() {
+  const newSchedule = {
+    scheduleId: Math.random().toString(36).substring(7),
+    alarmTime: DateTime.now()
+      .plus({
+        hours: newShockScheduleMinutes.value.hours,
+        minutes: newShockScheduleMinutes.value.minutes,
+      })
+      .toISO(),
+  };
+  registeredSchedules.state.value = [
+    ...registeredSchedules.state.value!,
+    newSchedule,
+  ];
+}
 </script>
 
 <template>
@@ -42,46 +65,93 @@ async function onScheduleClick(scheduleId: string) {
     <ShockPower />
     <Separator class="h-px my-8 w-full bg-base-300" />
     <div class="text-center place-items-center">
-      <template v-for="(schedule, i) in schedules" :key="i">
-        <div
-          class="flex flex-row shadow-lg"
-          :class="{
-            'animate-[pulse_1s_ease-in-out_infinite] border-2 border-warning rounded-lg p-4 ':
-              DateTime.fromISO(schedule.alarmTime)
-                .diffNow()
-                .as(`milliseconds`) < 0,
-          }"
+      <div class="flex flex-row mb-8 shadow p-4">
+        <NumberFieldRoot
+          id="age"
+          v-model="newShockScheduleMinutes.hours"
+          class="text-sm flex flex-row items-center mr-8"
+          :min="0"
+          :default-value="5"
         >
-          <div class="flex flex-col mb-2 p-4 rounded-lg mr-8">
-            <div class="text-xl font-bold mb-4">
-              Scheduled Time:
-              {{ DateTime.fromISO(schedule.alarmTime).toFormat("HH:mm") }}
-            </div>
-            <div class="text-lg font-bold">
-              Shock In:
-              {{
-                DateTime.fromISO(schedule.alarmTime)
-                  .diff(currentTime, [
-                    "hours",
-                    "minutes",
-                    "seconds",
-                    "milliseconds",
-                  ])
-                  .toHuman()
-                  .split(", ")
-                  .splice(0, 3)
-                  .join(", ")
-              }}
-            </div>
+          <div class="mr-4">Schock In:</div>
+          <div class="mt-1 flex items-center border rounded-md">
+            <NumberFieldDecrement class="p-2 disabled:opacity-20">
+              <icon name="mdi:minus" />
+            </NumberFieldDecrement>
+            <NumberFieldInput
+              class="bg-transparent w-20 tabular-nums focus:outline-0 p-1"
+            />
+            <NumberFieldIncrement class="p-2 disabled:opacity-20">
+              <icon name="mdi:plus" />
+            </NumberFieldIncrement>
           </div>
-          <button
-            class="btn"
-            @click="() => onScheduleClick(schedule.scheduleId)"
+          <div class="ml-4">hours</div>
+        </NumberFieldRoot>
+        <NumberFieldRoot
+          id="age"
+          v-model="newShockScheduleMinutes.minutes"
+          class="text-sm flex flex-row items-center mr-8"
+          :min="0"
+          :default-value="5"
+        >
+          <div class="mt-1 flex items-center border rounded-md">
+            <NumberFieldDecrement class="p-2 disabled:opacity-20">
+              <icon name="mdi:minus" />
+            </NumberFieldDecrement>
+            <NumberFieldInput
+              class="bg-transparent w-20 tabular-nums focus:outline-0 p-1"
+            />
+            <NumberFieldIncrement class="p-2 disabled:opacity-20">
+              <icon name="mdi:plus" />
+            </NumberFieldIncrement>
+          </div>
+          <div class="ml-4">minutes</div>
+        </NumberFieldRoot>
+        <button class="btn" @click="onScheduleAddClick">Add</button>
+      </div>
+      <template v-if="schedules.length > 0">
+        <template v-for="(schedule, i) in schedules" :key="i">
+          <div
+            class="flex flex-row shadow-lg mb-2"
+            :class="{
+              'animate-[pulse_1s_ease-in-out_infinite] border-2 border-warning rounded-lg p-4 ':
+                DateTime.fromISO(schedule.alarmTime)
+                  .diffNow()
+                  .as(`milliseconds`) < 0,
+            }"
           >
-            Cancel
-          </button>
-        </div>
+            <div class="flex flex-col mb-2 p-4 rounded-lg mr-8">
+              <div class="text-xl font-bold mb-4">
+                Scheduled Time:
+                {{ DateTime.fromISO(schedule.alarmTime).toFormat("HH:mm") }}
+              </div>
+              <div class="text-lg font-bold">
+                Shock In:
+                {{
+                  DateTime.fromISO(schedule.alarmTime)
+                    .diff(currentTime, [
+                      "hours",
+                      "minutes",
+                      "seconds",
+                      "milliseconds",
+                    ])
+                    .toHuman()
+                    .split(", ")
+                    .splice(0, 3)
+                    .join(", ")
+                }}
+              </div>
+            </div>
+            <button
+              class="btn"
+              @click="() => onScheduleClick(schedule.scheduleId)"
+            >
+              Cancel
+            </button>
+          </div>
+        </template>
       </template>
+      <template v-else> No schedules found </template>
     </div>
   </div>
 </template>
